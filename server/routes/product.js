40f5b249b03edfaf98a248ddd00164c4a647b06c
abdulraim <abdulrahimcisco92@gmail.com>
2022-01-03 19:24:28 +0300

    adding get query route

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,15 +2,22 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
 const isAdmin = require("../middleware/isAdmin");
-const { postProduct, updateProduct, deleteProduct, getProduct } = require("../controllers/product");
+const { 
+        postProduct, 
+        updateProduct, 
+        deleteProduct, 
+        getProduct,
+        getAllProducts 
+      } = require("../controllers/product");
 
 router.route("/").post(protect, isAdmin, postProduct);
+
+router.route("/").get(protect, getAllProducts);
 
 router.route("/:id").patch(protect, isAdmin, updateProduct);
 
 router.route("/:id").delete(protect, isAdmin, deleteProduct);
 
 router.route("/:id").get(protect, getProduct);
-
 
 module.exports = router;
